d4ec3178797fade0d2e924ed91b20a8904305507
HydroCarbon <zhenfeng.zou1998@gmail.com>
2022-12-12 14:47:57 +0800

    Add common class

---
+++ rush-java-algorithm-common/src/main/java/org/hydrocarbonx/rush/TreeNode.java
@@ -0,0 +1,15 @@
+package org.hydrocarbonx.rush;
+
+/**
+ * @author HydroCarbon
+ */
+@SuppressWarnings({"java:S1104", "unused"})
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode(int x) {
+        this.val = x;
+    }
+}
