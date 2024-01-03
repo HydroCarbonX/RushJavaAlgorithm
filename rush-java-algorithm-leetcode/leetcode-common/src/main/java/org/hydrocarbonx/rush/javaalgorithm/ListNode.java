02db3dc558e43aa4813dfca1e167bcbaba677db2
HydroCarbon <zhenfeng.zou1998@gmail.com>
2024-01-03 15:40:04 +0800

    :bulb: add common ListNode

---
+++ rush-java-algorithm-leetcode/leetcode-common/src/main/java/org/hydrocarbonx/rush/javaalgorithm/ListNode.java
@@ -0,0 +1,24 @@
+package org.hydrocarbonx.rush.javaalgorithm;
+
+/**
+ * @author zouzhenfeng
+ * @since 2024-01-03
+ */
+public class ListNode {
+
+    int val;
+
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
