23847a68b56b9e90cc15397d20686ba4596c0429
HydroCarbon <zhenfeng.zou1998@gmail.com>
2024-02-04 20:28:49 +0800

    :pushpin: add 292

---
+++ rush-java-algorithm-leetcode/leetcode-easy/src/main/java/org/hydrocarbonx/rush/javaalgorithm/_292.java
@@ -0,0 +1,12 @@
+package org.hydrocarbonx.rush.javaalgorithm;
+
+/**
+ * @author HydroCarbon
+ * @since 2024-02-04
+ */
+public class _292 {
+
+    public boolean canWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
